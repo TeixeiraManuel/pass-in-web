8a6c96f5aba1566a64db0d0bff748018cf9e152b
CristecPro <procristec@gmail.com>
2024-04-02 10:28:47 +0100

    primeira parte do layout concluido

---
+++ README.md
@@ -0,0 +1,13 @@
+## Pass In Web
+
+
+
+## Como Executar o Projeto
+
+1. Certifique-se de ter o ambiente de desenvolvimento React  configurado em seu sistema.
+
+2. Clone este repositório em seu ambiente de desenvolvimento.
+
+3. Navegue até o diretório do projeto e execute `npm install` ou `yarn install` para instalar todas as dependências.
+
+4. Após a instalação das dependências, você pode iniciar o aplicativo executando `npm run dev`.
---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import nlwUniteIcon from "../assets/nlw-unite-icon.svg"
+export function Header(){
+  return(
+    <div className="flex items-center gap-5 py-2">
+      <img src={nlwUniteIcon} alt="logotipo do nlw" />
+      <nav className="flex items-center gap-5">
+        <a href="" className="font-medium text-sm text-zinc-300">Eventos</a>
+        <a href="" className="font-medium text-sm">Participantes</a>
+      </nav>
+    </div>
+  )
+}
